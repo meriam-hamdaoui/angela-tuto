7a2efdfe2d24237b837329d2e5f508dc2ab71044
meriam <hamdaoui.meriam85@gmail.com>
2022-04-02 11:50:53 +0200

    footer

---
+++ src/components/Notes/Note.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Note() {
+  return (
+    <div className='Note'>
+      <h1 className='noteh'></h1>
+      <p className='notep'></p>
+    </div>
+  )
+}
+
+export default Note;
